949c3553581c13a7e1a3c719fb8ee4761dda7d5f
jpsoares01 <jpsoares111@hotmail.com>
2023-07-03 09:38:45 -0300

    adicionando posts

--- Front-end/src/app/new-post/new-post.component.ts
+++ Front-end/src/app/new-post/new-post.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { NewPost } from '../service/post/new-post';
-import { PostService } from '../service/post/post.service';
-import { AsyncPipe, CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
+
+import { MatAutocompleteModule, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatChipInputEvent, MatChipsModule} from '@angular/material/chips';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { PostService } from '../service/post/post.service';
 import { MatButtonModule } from '@angular/material/button';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { NewPost } from '../service/post/new-post';
+import { Observable } from 'rxjs';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import {map, startWith} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-new-post',
@@ -26,11 +33,65 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
     MatAutocompleteModule,
     ReactiveFormsModule,
     AsyncPipe,
-
+    MatChipsModule
   ]
 })
 
 export class NewPostComponent {
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  fruitCtrl = new FormControl('');
+  filteredFruits: Observable<string[]>;
+  fruits: string[] = ['Lemon'];
+  allFruits: string[] = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
+
+  @ViewChild('fruitInput') fruitInput: ElementRef<HTMLInputElement> = {} as ElementRef;;
+
+  announcer = inject(LiveAnnouncer);
+  
+  // constructor () { }
+  constructor(private service : PostService) {
+    this.filteredFruits = this.fruitCtrl.valueChanges.pipe(
+      startWith(null),
+      map((fruit: string | null) => (fruit ? this._filter(fruit) : this.allFruits.slice())),
+    );
+  }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.fruits.push(value);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+
+    this.fruitCtrl.setValue(null);
+  }
+
+  remove(fruit: string): void {
+    const index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+
+      this.announcer.announce(`Removed ${fruit}`);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.fruits.push(event.option.viewValue);
+    this.fruitInput.nativeElement.value = '';
+    this.fruitCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allFruits.filter(fruit => fruit.toLowerCase().includes(filterValue));
+  }
+
 
   tempJwt = sessionStorage.getItem("UserId") ?? ''
   PostRegister : NewPost = 
@@ -40,8 +101,6 @@ export class NewPostComponent {
     postData : "",
     title : ""
   }
-
-  constructor (private service : PostService) { }
 
   newPost()
   {
